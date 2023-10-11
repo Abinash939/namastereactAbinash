99708047a3cd12b54606b223b81a531edc420ee8
Abinash939 <112938942+Abinash939@users.noreply.github.com>
2023-10-11 23:57:52 +0530

    1st eposode

---
+++ rect.js
@@ -0,0 +1,13 @@
+const head = React.createElement("div",{id: "parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag child 1 "),
+        React.createElement("h2",{},"hi i am h2 tag child 1"),
+    ]),
+    React.createElement("div",{id: "child"},[
+        React.createElement("h1",{},"hi am h1 tag from child 2"),
+        React.createElement("h2",{},"hi am h2 from child 2"),
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head)
